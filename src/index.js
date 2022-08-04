ab883dca99e631115b2b7b3248b721e7d67edec0
Bryan Soriano <bryansoriano@knights.ucf.edu>
2022-08-04 15:43:10 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,3 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
-
-const AnimatedLetters = ({ letterClass, strArray, idx }) => {
-    return (
-        <span>
-          	strArray.map((char,i) => (
-            <span key={char + i} className={`${letterClass} _${i + idx}`}>
-                {char}
-            </span>
-            }
-
-        </span>
-        )
-}
